9b61cfdb0329c84ffd06143ae202343038b70c44
werto87 <huhulasmichinruhe@web.de>
2024-02-08 11:36:23 +0900

    minimal example

--- src/hello.hxx
+++ src/hello.hxx
@@ -2,8 +2,6 @@
 #define D9077A7B_F0F8_4687_B460_C6D43C94F8AF
 #include <boost/asio/awaitable.hpp>
 
-// boost::asio::awaitable<void> helloWorld ();
-
-int abc ();
+int abc();
 
 #endif /* D9077A7B_F0F8_4687_B460_C6D43C94F8AF */
