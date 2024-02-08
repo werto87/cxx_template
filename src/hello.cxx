986733a6914d20fd2559da888900a7a43b489efa
werto87 <huhulasmichinruhe@web.de>
2024-02-08 11:38:42 +0900

    minimal example

--- src/hello.cxx
+++ src/hello.cxx
@@ -2,17 +2,12 @@
 #include <boost/asio.hpp>
 #include <boost/asio/experimental/awaitable_operators.hpp>
 
-boost::asio::awaitable<void>
-helloWorld ()
-{
+boost::asio::awaitable<void> helloWorld() {
   using namespace boost::asio::experimental::awaitable_operators;
   auto ioContext = boost::asio::io_context{};
-  boost::asio::ip::tcp::socket socket (ioContext);
-  auto test = socket.async_connect ({}, boost::asio::use_awaitable) && socket.async_connect ({}, boost::asio::use_awaitable);
+  boost::asio::ip::tcp::socket socket(ioContext);
+  auto test = socket.async_connect({}, boost::asio::use_awaitable) &&
+              socket.async_connect({}, boost::asio::use_awaitable);
 }
 
-int
-abc ()
-{
-  return 42;
-}
+int abc() { return 42; }
