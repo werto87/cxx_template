6735f3344e9c477b5df24f1aead48dd8e19c0ced
werto87 <huhulasmichinruhe@web.de>
2024-08-02 21:21:25 +0200

    simple main example

--- src/hello.hxx
+++ src/hello.hxx
@@ -1,6 +1,3 @@
-#ifndef D9077A7B_F0F8_4687_B460_C6D43C94F8AF
-#define D9077A7B_F0F8_4687_B460_C6D43C94F8AF
+#pragma once
 #include <string>
 std::string helloWorld ();
-
-#endif /* D9077A7B_F0F8_4687_B460_C6D43C94F8AF */
